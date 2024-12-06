c0608fe2c8efbe6096600d7969e53a492897e6fd
Mikkel Sørensen <m_hooge@hotmail.com>
2024-12-06 12:01:04 +0100

    Day 6 done with some brute force action.

---
+++ Day6/Day6.fs
@@ -0,0 +1,81 @@
+module Day6
+
+let guards = ['^'; '>'; 'v'; '<']
+
+let rec tracePath x y dx dy (arr: char array2d) =
+    if x < 0 || x >= Array2D.length1 arr || y < 0 || y >= Array2D.length2 arr then arr
+    else
+    match arr[y,x] with
+    | '#' -> tracePath (x - dx) (y - dy) (dy * -1) (dx * 1) arr
+    | '.' | 'X' ->
+        arr[y,x] <- 'X'
+        tracePath (x + dx) (y + dy) dx dy arr
+    | _ -> arr
+
+
+let part1 (lines: string list) =
+    let w = lines[0].Length
+    let h = lines.Length
+    let charArr = List.map (fun (s: string) -> s.ToCharArray()) lines
+    let arr = Array2D.init h w (fun x y -> charArr[x][y])
+
+    let y = List.findIndex (fun a -> (Array.tryFind (fun v -> List.contains v guards) a).IsSome) charArr
+    let x = Array.findIndex (fun v -> List.contains v guards) charArr[y]
+    let dx = (
+        match charArr[y][x] with
+        | '>' -> 1
+        | '<' -> -1
+        | _ -> 0
+    )
+    let dy = (
+        match charArr[y][x] with
+        | 'v' -> 1
+        | '^' -> -1
+        | _ -> 0
+    )
+
+    arr[y,x] <- 'X'
+    let newArr = tracePath x y dx dy arr
+    Seq.sumBy (fun c -> if c = 'X' then 1 else 0) (Seq.cast newArr)
+
+let rec tracePathCycle x y dx dy (arr: char array2d) s =
+    if x < 0 || x >= Array2D.length1 arr || y < 0 || y >= Array2D.length2 arr then 0
+    elif Set.contains (x, y, dx, dy) s then 1
+    else
+    match arr[y,x] with
+    | '#' -> tracePathCycle (x - dx) (y - dy) (dy * -1) (dx * 1) arr s
+    | '.' | 'X' ->
+        arr[y,x] <- 'X'
+        tracePathCycle (x + dx) (y + dy) dx dy arr (Set.add (x, y, dx, dy)  s)
+    | _ -> 0
+
+let tryStartPoint sx sy x y dx dy (charArr: char array2d) =
+    let total = Array2D.length1 charArr * Array2D.length2 charArr
+    let progress = x + y * (Array2D.length2 charArr)
+    if List.contains (charArr[y, x]) guards then 0
+    else 
+        charArr[y,x] <- '#'
+        tracePathCycle sx sy dx dy charArr Set.empty
+
+let part2 (lines: string list) =
+    let charArr = List.map (fun (s: string) -> s.ToCharArray()) lines
+    let sy = List.findIndex (fun a -> (Array.tryFind (fun v -> List.contains v guards) a).IsSome) charArr
+    let sx = Array.findIndex (fun v -> List.contains v guards) charArr[sy]
+    let w = charArr[0].Length
+    let h = charArr.Length
+    let arr = Array2D.init h w (fun x y -> charArr[x][y])
+    let dx = (
+        match charArr[sy][sx] with
+        | '>' -> 1
+        | '<' -> -1
+        | _ -> 0
+    )
+    let dy = (
+        match charArr[sy][sx] with
+        | 'v' -> 1
+        | '^' -> -1
+        | _ -> 0
+    )
+    arr[sy,sx] <- 'X'
+
+    List.sumBy (fun (y, a) -> Array.sumBy (fun (x, _) -> tryStartPoint sx sy x y dx dy (Array2D.copy arr)) (Array.indexed a)) (List.indexed charArr)
--- Main.fs
+++ Main.fs
@@ -5,7 +5,8 @@ let days: ((string list -> int) list) list = [
     [Day2.part1; Day2.part2];
     [Day3.part1; Day3.part2];
     [Day4.part1; Day4.part2];
-    [Day5.part1; Day5.part2]
+    [Day5.part1; Day5.part2];
+    [Day6.part1; Day6.part2]
 ]
 
 [<EntryPoint>]
